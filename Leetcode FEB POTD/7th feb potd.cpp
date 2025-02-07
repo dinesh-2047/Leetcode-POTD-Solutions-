7b598e1b8b304b8b60ef7b35718a513346269ecc
dinesh-2047 <dinesh07bhardwaj@gmail.com>
2025-02-07 19:14:43 +0530

    7th feb potd added 3160. Find the Number of Distinct Colors Among the Balls

---
+++ Leetcode FEB POTD/7th feb potd.cpp
@@ -0,0 +1,35 @@
+// 3160. Find the Number of Distinct Colors Among the Balls
+
+
+class Solution {
+    public:
+        vector<int> queryResults(int limit, vector<vector<int>>& queries) {
+            int n = queries.size();
+    
+            vector<int>ans(n);
+    
+            unordered_map<int, int>colormp; //color ->count
+    
+           unordered_map<int,int>ballmp; // ball -> color
+    
+            for(int i =0 ; i<n; i++){
+                int ball = queries[i][0];
+                int color = queries[i][1];
+                  
+                if(ballmp.count(ball)){//already colored ball
+                      int prevColor = ballmp[ball];
+                      colormp[prevColor]--;
+    
+                      if(colormp[prevColor] == 0){
+                        colormp.erase(prevColor);
+                      }
+                }
+                ballmp[ball] = color;
+                colormp[color]++;
+    
+                ans[i] = colormp.size();
+    
+            }
+            return ans;
+        } 
+    };
