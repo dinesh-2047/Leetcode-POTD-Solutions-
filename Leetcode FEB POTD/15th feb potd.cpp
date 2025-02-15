1862958652f55983b3ee14f89a51ba9b51c04d19
dinesh-2047 <dinesh07bhardwaj@gmail.com>
2025-02-15 19:26:22 +0530

    15th feb 3rd approach

--- Leetcode FEB POTD/15th feb potd.cpp
+++ Leetcode FEB POTD/15th feb potd.cpp
@@ -73,3 +73,20 @@ class Solution {
             return punishmentNum;
         }
     };
+
+
+    // Approach 3 -> good time and space complexity but not good logically 
+
+    class Solution {
+        public:
+            int punishmentNumber(int n) {
+                vector<int> arr = {1,9,10,36,45,55,82,91,99,100,235,297,369,370,379,414,
+                                   657,675,703,756,792,909,918,945,964,990,991,999,1000};
+                int sum = 0;
+                for(int i = 0; i < arr.size(); i++) {
+                    if(arr[i] <= n) sum += arr[i] * arr[i];
+                    else break;
+                }
+                return sum;
+            }
+        };
