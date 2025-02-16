be580387c0178a2cd095ac4722599d1fae6a1167
dinesh-2047 <dinesh07bhardwaj@gmail.com>
2025-02-16 17:16:41 +0530

    15th feb potd solution 1718. Construct the Lexicographically Largest Valid Sequence

---
+++ Leetcode FEB POTD/15th feb potd .cpp
@@ -0,0 +1,50 @@
+// 1718. Construct the Lexicographically Largest Valid Sequence 
+
+
+class Solution {//backtracking 
+    public:
+    bool solve(int i , int n , vector<int>&result , vector<bool>&used){
+    
+              if(i>= result.size()) return true; //guranted answer - we are filling largest numbers first;
+    
+              if(result[i] != -1) return solve(i+1, n , result , used);
+    
+              for(int num = n ; num >=1 ; num--){
+                if(used[num]) continue; 
+    
+                //try
+                used[num] = true; 
+                result[i] = num ;
+    
+                if(num==1){
+                    if(solve(i+1, n , result , used) == true) return true; 
+                }
+                else {
+                         int j = result[i] + i;
+                         if(j<result.size() && result[j] == -1){
+                            result[j] = num;
+                            if(solve(i+1, n , result , used ) == true){
+                                return true; 
+                            }
+                            result[j] = -1;
+                         }
+                }
+    
+                
+                used[num]= false;
+                result[i] = -1;
+              }
+              return false; 
+    }
+    
+    
+        vector<int> constructDistancedSequence(int n) {
+            
+            vector<int>result (2*n-1 , -1);
+    
+            vector<bool>used(n+1, false);
+    
+            solve(0, n, result , used);
+            return result; 
+        }
+    };
