f233a53ca85b52cad86c61de680e9a8c826388c7
dinesh-2047 <dinesh07bhardwaj@gmail.com>
2025-02-20 15:57:54 +0530

    20th feb potd 1980. Find Unique Binary String approach 1 added

---
+++ Leetcode FEB POTD/20th feb potd.cpp
@@ -0,0 +1,23 @@
+// 1980. Find Unique Binary String 
+
+
+
+// Approach 1 :  Cantor's Diagonalization (Greedy Approach)
+
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        int n = nums.size();
+         string ans  = "";
+
+        for(int i = 0 ; i<n; i++){
+            char ch = nums[i][i];
+
+            if(ch =='0'){
+                ans += '1';
+            }
+            else ans += '0';
+        }
+        return ans; 
+    }
+};
