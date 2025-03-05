fccafef3b86f602d99efc889fe692ea549004b65
dinesh-2047 <dinesh07bhardwaj@gmail.com>
2025-03-05 18:00:30 +0530

    5th march potd solution added 2579. Count Total Number of Colored Cellso

---
+++ Leetcode March potd/5th march potd.cpp
@@ -0,0 +1,42 @@
+// 2579. Count Total Number of Colored Cells
+
+//Approach-1 (Using simulation)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+    public:
+        long long coloredCells(int n) {
+            int t = 1;
+            long long cells = 1;
+    
+            while(t <= n) {
+                cells += (4 * (t-1));
+                t++;
+            }
+    
+            return cells;
+        }
+    };
+    
+    
+    //Approach-2 (Using Mathematical derivation)
+    //T.C : O(1)
+    //S.C : O(1)
+    class Solution {
+    public:
+        long long coloredCells(int n) {
+            return 1 + (2 * (long long)(n-1) * n);
+        }
+    };
+
+    
+    // Approach 3 :- 
+    // T.C - O(1);
+    // S.C - O(1);
+
+    class Solution {
+        public:
+            long long coloredCells(int n) {
+                return pow(n, 2 ) + pow(n-1, 2);
+            }
+        };
