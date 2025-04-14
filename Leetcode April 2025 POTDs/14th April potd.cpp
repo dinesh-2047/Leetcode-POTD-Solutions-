3cc1aa61ca0f6fb65a2c6103ec2717ac8109cb34
dinesh-2047 <dinesh07bhardwaj@gmail.com>
2025-04-14 17:47:24 +0530

    1534. Count Good Triplets 14th april potd solution added

---
+++ Leetcode April 2025 POTDs/14th April potd.cpp
@@ -0,0 +1,45 @@
+// 1534. Count Good Triplets
+
+
+// Approach 1 
+
+class Solution {
+    public:
+        int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+            int n = arr.size();
+            int ans = 0; 
+    
+            for(int i = 0 ; i<n-2; i++){
+                for(int j = i+1; j<n-1; j++){
+                    for(int k = j+1; k<n; k++){
+                         if(abs(arr[i]-arr[j]) <=a && abs(arr[j]-arr[k]) <=b && abs(arr[i]-arr[k] ) <=c) ans++;
+                    }
+                }
+            }
+            return ans; 
+        }
+    
+    };
+
+
+    // Approach 2 
+
+    class Solution {
+        public:
+            int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+                int n = arr.size();
+                int ans = 0; 
+        
+                for(int i = 0 ; i<n-2; i++){
+                    for(int j = i+1; j<n-1; j++){
+                        if(abs(arr[i]-arr[j]) <=a){
+                        for(int k = j+1; k<n; k++){
+                             if(  abs(arr[j]-arr[k]) <=b && abs(arr[i]-arr[k] ) <=c) ans++;
+                        }
+                    }
+                    }
+                }
+                return ans; 
+            }
+        
+        };
