ae29647ef150fa1d41e5fd4e6ee4d9319e1893d3
dinesh-2047 <dinesh07bhardwaj@gmail.com>
2025-05-11 20:00:36 +0530

    easy 1550. Three Consecutive Odds 11th may

---
+++ Easy/11th May - 1550. Three Consecutive Odds.cpp
@@ -0,0 +1,34 @@
+// 1550. Three Consecutive Odds
+
+// Approach  1
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) { 
+        int n = arr.size();
+
+        for(int i = 0 ; i<n-2; i++){
+           if(arr[i]%2==1 && arr[i+1]%2==1 && arr[i + 2]%2 == 1) return true; 
+        }
+        return false; 
+    }
+};
+
+
+
+
+// Approach 2/
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) { 
+        int n = arr.size();
+          
+          int odds = 0 ;
+        for(int i = 0 ; i<n; i++){
+             if(arr[i]%2==1) odds++;
+             else odds = 0;
+             if(odds == 3 ) return true; 
+        }
+   
+        return false; 
+    }
+}; 
