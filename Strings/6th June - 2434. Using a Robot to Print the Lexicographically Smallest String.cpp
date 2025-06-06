a9b2119d61a3f9836b4a31f6ca8e44d3f77a6d5c
dinesh-2047 <dinesh07bhardwaj@gmail.com>
2025-06-06 18:05:42 +0530

    6th june 2434. Using a Robot to Print the Lexicographically Smallest String

---
+++ Strings/6th June - 2434. Using a Robot to Print the Lexicographically Smallest String.cpp
@@ -0,0 +1,44 @@
+// 2434. Using a Robot to Print the Lexicographically Smallest String
+
+
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    string robotWithString(string s) {
+        int n = s.length();
+
+        vector<char> minCharToRight(n); //you can store index of those characters instead of storing characters
+
+        minCharToRight[n-1] = s[n-1];
+        for(int i = n-2; i >= 0; i--) {
+            minCharToRight[i] = min(s[i], minCharToRight[i+1]);
+        }
+
+
+        string t = "";
+        string paper = "";
+
+        int i = 0; 
+        while(i < n) {
+            t.push_back(s[i]);
+
+            char minChar = (i+1 < n) ? minCharToRight[i+1] : s[i];
+
+            while(!t.empty() && t.back() <= minChar) {
+                paper += t.back();
+                t.pop_back();
+            }
+
+            i++;
+        }
+
+        //fill the remaining characters
+        while(!t.empty()) {
+            paper += t.back();
+            t.pop_back();
+        }
+
+        return paper;
+    }
+};
