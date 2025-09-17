cf48048ead69af878ff334a50b8e1e954630ef75
dinesh-2047 <dinesh07bhardwaj@gmail.com>
2025-09-18 00:56:36 +0530

    2353. Design a Food Rating System  design 17th september

---
+++ Design/17th September - 2353. Design a Food Rating System.cpp
@@ -0,0 +1,38 @@
+// 2353. Design a Food Rating System
+
+
+class FoodRatings {
+public:
+    unordered_map<string, int> food_rating;
+    unordered_map<string, string> food_cuisine;
+    unordered_map<string, set<pair<int, string>>> cuisine_rating_food;
+
+    FoodRatings(vector<string>& foods, vector<string>& cuisines,
+                vector<int>& ratings) {
+        for (int i = 0; i < foods.size(); i++) {
+            food_rating[foods[i]] = ratings[i];
+            food_cuisine[foods[i]] = cuisines[i];
+            cuisine_rating_food[cuisines[i]].insert({-ratings[i], foods[i]});
+        }
+    }
+
+    void changeRating(string food, int newRating) {
+        string cuisine = food_cuisine[food];
+        int prevRating = food_rating[food];
+        
+        food_rating[food] = newRating; 
+        cuisine_rating_food[cuisine].erase({-prevRating, food});
+        cuisine_rating_food[cuisine].insert({-newRating, food});
+    }
+
+    string highestRated(string cuisine) {
+        return cuisine_rating_food[cuisine].begin()->second;
+    }
+};
+
+/**
+ * Your FoodRatings object will be instantiated and called as such:
+ * FoodRatings* obj = new FoodRatings(foods, cuisines, ratings);
+ * obj->changeRating(food,newRating);
+ * string param_2 = obj->highestRated(cuisine);
+ */
