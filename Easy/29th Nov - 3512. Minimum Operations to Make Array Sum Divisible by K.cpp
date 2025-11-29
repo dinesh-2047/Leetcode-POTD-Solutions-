32a8e4e657120cff739ba561856df830e35b3d39
dinesh-2047 <dinesh07bhardwaj@gmail.com>
2025-11-30 02:51:35 +0530

    3512 : minimum operations to make array sum divisible by k

--- Easy/29th Nov - 3512. Minimum Operations to Make Array Sum Divisible by K.cpp
+++ Easy/29th Nov - 3512. Minimum Operations to Make Array Sum Divisible by K.cpp
@@ -1,3 +1,10 @@
 // 3512. Minimum Operations to Make Array Sum Divisible by K
 
 
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+      return  accumulate(begin(nums), end(nums), 0)%k;
+    
+    }
+};
