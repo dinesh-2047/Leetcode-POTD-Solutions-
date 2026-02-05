5fbbcf0d5138e4c65bd7fb8555372dfb5fad0adf
dinesh-2047 <dinesh07bhardwaj@gmail.com>
2026-02-05 23:00:04 +0530

    3379. Transformed Array 5th feb

---
+++ Easy/5th Feb 2026 - 3379. Transformed Array.cpp
@@ -0,0 +1,15 @@
+// 3379. Transformed Array
+
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<int>result(n);
+
+        for(int i = 0; i < n; i++){
+             result[i] = nums[(((i+nums[i])%n)+n)%n];
+        }
+        return result; 
+    }
+};
