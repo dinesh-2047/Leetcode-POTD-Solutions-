5cf4e4f6a9cb18f5c50da04c4a3f6539764f4de8
dinesh-2047 <dinesh07bhardwaj@gmail.com>
2026-03-27 16:26:21 +0530

    2946. Matrix Similarity After Cyclic Shifts 27th march 26

---
+++ Easy/27th March 26 . Matrix Similarity After Cyclic Shifts.cpp
@@ -0,0 +1,19 @@
+// 2946. Matrix Similarity After Cyclic Shifts
+
+class Solution {
+public:
+    bool areSimilar(vector<vector<int>>& mat, int k) {
+        int m = mat.size();
+        int n = mat[0].size();
+        
+        vector<vector<int>>temp = mat; 
+
+        for(int i = 0 ;i  < m; i++){
+            for(int j = 0 ; j  < n; j++){
+                if(mat[i][j] != mat[i][(j+k)%n]) return false; 
+            }
+        }
+
+        return true; 
+    }
+};
