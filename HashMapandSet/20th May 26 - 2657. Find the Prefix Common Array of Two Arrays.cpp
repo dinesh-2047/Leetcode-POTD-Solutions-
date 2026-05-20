98acfa0a9334b9e9d77977117912e443dfc3b8f0
dinesh-2047 <dinesh07bhardwaj@gmail.com>
2026-05-20 23:37:35 +0530

    2657. Find the Prefix Common Array of Two Arrays 20th may 26

---
+++ HashMapandSet/20th May 26 - 2657. Find the Prefix Common Array of Two Arrays.cpp
@@ -0,0 +1,26 @@
+// 2657. Find the Prefix Common Array of Two Arrays
+
+class Solution {
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        int n = A.size();
+
+        unordered_map<int, int>mp ; 
+        int count = 0 ; 
+
+        vector<int> result(n);
+
+        for(int i = 0 ; i < n; i++){
+            mp[A[i]]++;
+            if(mp[A[i]] == 2){
+                count++;
+            }
+            mp[B[i]]++;
+            if(mp[B[i]] == 2){
+                count++;
+            }
+            result[i] = count; 
+        }
+        return result; 
+    }
+};
