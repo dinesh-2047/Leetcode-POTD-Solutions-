b2ca67b1d410e0860d716f891e6c5a18f9466c87
dinesh-2047 <dinesh07bhardwaj@gmail.com>
2026-05-21 15:21:24 +0530

    3043. Find the Length of the Longest Common Prefix 21st may 26

---
+++ Greedy/21st May 26 - 3043. Find the Length of the Longest Common Prefix.cpp
@@ -0,0 +1,35 @@
+// 3043. Find the Length of the Longest Common Prefix
+
+
+
+class Solution {
+public:
+    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+        int  n = arr1.size();
+        int m  = arr2.size();
+        unordered_map<int , int>mp; 
+
+        for(int i = 0 ; i  < n; i++){
+            string s = to_string(arr1[i]);
+            for(int len = 1; len <= s.length() ; len++){
+                string str = s.substr(0, len);
+                int num = stoi(str);
+                mp[num]++;
+            }
+        }
+          int result =0 ; 
+        for(int i = 0 ; i  < m; i++){
+            string s = to_string(arr2[i]);
+            for(int len = 1; len <= s.length() ; len++){
+                string str = s.substr(0, len);
+                int num = stoi(str);
+                if(mp.count(num)){
+                    result = max(result, (int)str.length());
+                }
+            }
+        }
+        return result;
+
+
+    }
+};
